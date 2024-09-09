64f9e9f0ca16ac2f90015245e1ed690bc84a0df2
Evelyn_ Hernandez <eh22@usf.edu>
2024-09-08 23:03:49 -0400

    Test functions with sample sales data

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -52,6 +52,20 @@ function generatePerformanceReport(salespeople);
 `Performance Rating:` ${performanceRating}
 
 `Top Performer:` ${topPerformer}
-`Bottom Performer:` ${bottomPerformer}
+`Bottom Performer:` ${bottomPerformer} 
 
 console.log(generatePerformanceReport(salespeople));
+
+//Task 4: Test your functions with sample data
+
+const salesData = [
+
+  { name: 'Alice', sales: [12000, 15000, 13000] },
+    
+  { name: 'Bob', sales: [7000, 6000, 7500] },
+    
+  { name: 'Charlie', sales: [3000, 4000, 3500] },
+    
+  { name: 'Diana', sales: [9000, 8500, 9200] },
+    
+    ];
