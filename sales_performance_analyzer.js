f473fb3ba5a6cdb068ea8114f93e3da7776dc473
Evelyn_ Hernandez <eh22@usf.edu>
2024-09-08 21:59:04 -0400

    Implement calculateAverageSales function

---
+++ sales_performance_analyzer.js
@@ -0,0 +1,7 @@
+//Task 1: Create a Function to Calculate Average Sales
+//Function Declaration - Calculate average sales
+let sales = [50,200,30];
+function calculateAverageSales (sales, quantity){
+    sum += sales;
+return sum / sales.length };
+console.log(`Average Sales:$${calculateAverageSales}`);
