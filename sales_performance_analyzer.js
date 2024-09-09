c853139feba6681cbfa06b9d002bbed512de0e75
Evelyn_ Hernandez <eh22@usf.edu>
2024-09-08 22:15:54 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -5,3 +5,16 @@ function calculateAverageSales (sales, quantity){
     sum += sales;
 return sum / sales.length };
 console.log(`Average Sales:$${calculateAverageSales}`);
+
+//Task 2: Create a Function to Determine Performance Rating
+function determinePerformanceRating (sales, calculateAverageSales){
+    return calculateAverageSales;
+}
+if (calculateAverageSales > 10000){
+console.log(`Excellent`);}
+else if (calculateAverageSales 7000 <= x <= 10000){
+    console.log(`Good`);}
+    else if (calculateAverageSales 4000 <= x <= 7000){
+        console.log(`Satisfactory`);}
+        else (calculateAverageSales < 4000 ){
+            console.log(`Needs Improvement`);}
